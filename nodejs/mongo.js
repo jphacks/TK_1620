5539a58be0a8cf809b9728aa447fcc08665ee3bb
idealexe <omega_8810@yahoo.co.jp>
2016-10-28 14:30:01 +0900

    ファイルを移動

--- nodejs/mongo.js
+++
@@ -1,6 +0,0 @@
-var MongoClient = require('mongodb').MongoClient,
-	settings = require('./settings');
-MongoClient.connect("mongodb://" + settings.host + "/" + settings.db, function(err, db) {
-	if (err) {return console.dir(err); }
-	console.log("connected to db");
-});
--- nodejs/settings.js
+++
@@ -1,3 +0,0 @@
-exports.port = 3777;
-exports.host = 'localhost';
-exports.db = 'nodedb';
