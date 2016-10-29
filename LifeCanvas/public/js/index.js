53bc5e57c67c85cf03a6ea04067b23e6d0ac8290
idealexe <omega_8810@yahoo.co.jp>
2016-10-30 01:47:13 +0900

    統合用のデータを作成

---
+++ LifeCanvas/public/js/index.js
@@ -0,0 +1,52 @@
+(function () {
+
+  var $zo;
+  var beta;
+  var gamma;
+  var alpha;
+  var motionX, motionY, motionZ;
+  var motionXprev=0, motionYprev=0, motionZprev=0;
+
+  $(function () {
+    $zo = $("#zo");
+    window.addEventListener("deviceorientation", deviceorientationHandler);
+    window.addEventListener("devicemotion", motionHandler);
+  });
+
+  function motionHandler(event){
+    motionX = event.acceleration.x;
+    motionY = event.acceleration.y;
+    motionZ = event.acceleration.z;
+
+    socket.emit('sensor_update', ["acceleration", motionX - motionXprev, motionY - motionYprev, motionZ - motionZprev]);
+
+    motionXprev = motionX;
+    motionYprev = motionY;
+    motionZprev = motionZ;
+  }
+  /**
+  *
+  * @param event
+  */
+  function deviceorientationHandler(event) {
+    //ジャイロセンサー情報取得
+    // X軸
+    beta = event.beta;
+    // Y軸
+    gamma = event.gamma;
+    // Z軸
+    alpha = event.alpha;
+    var html = "";
+    html += "X回転 : " + beta + "<br>";
+    html += "Y回転 : " + gamma + "<br>";
+    html += 'Z回転 : ' + alpha;
+    $("#debug").html(html);
+
+    socket.emit('sensor_update', ["orientation", beta, gamma, alpha]);
+
+    $zo.css({
+      "-webkit-transform": "rotateX(" + (180 + beta) + "deg) rotateY(" + (180 + gamma) + "deg) rotateZ(" + alpha + "deg)",
+      "transform": "rotateX(" + (180 + beta) + "deg) rotateY(" + (180 + gamma) + "deg) rotateZ(" + alpha + "deg)"
+    })
+  }
+})();
