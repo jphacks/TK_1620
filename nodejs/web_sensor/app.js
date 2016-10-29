a01ab2b4e0cb68f5b59dc432f29757f476a583fd
EC2 Default User <ec2-user@ip-172-31-19-42.ap-northeast-1.compute.internal>
2016-10-29 13:31:24 +0000

    change index.html directry

--- nodejs/web_sensor/app.js
+++ nodejs/web_sensor/app.js
@@ -12,11 +12,11 @@ var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.sendFile('/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3001, function(){
+  console.log('listening on *:3001');
 });
 
 // view engine setup
