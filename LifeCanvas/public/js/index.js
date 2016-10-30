4ba9bf36430d3aaa6568962ff528f24c6f1a5cce
idealexe <omega_8810@yahoo.co.jp>
2016-10-30 10:17:58 +0900

    イベントの更新間隔を設定

--- LifeCanvas/public/js/index.js
+++ LifeCanvas/public/js/index.js
@@ -6,6 +6,8 @@
   var alpha;
   var motionX, motionY, motionZ;
   var motionXprev=0, motionYprev=0, motionZprev=0;
+  var event_counter = 0;
+  var event_interval = 10;
 
   $(function () {
     $logo = $("#logo");
@@ -14,35 +16,41 @@
   });
 
   function motionHandler(event){
-    motionX = event.acceleration.x;
-    motionY = event.acceleration.y;
-    motionZ = event.acceleration.z;
+    if (event_counter%event_interval == 0){
+      motionX = event.acceleration.x;
+      motionY = event.acceleration.y;
+      motionZ = event.acceleration.z;
 
-    socket.emit('sensor_update', ["acceleration", motionX - motionXprev, motionY - motionYprev, motionZ - motionZprev]);
+      socket.emit('sensor_update', ["acceleration", motionX - motionXprev, motionY - motionYprev, motionZ - motionZprev]);
 
-    motionXprev = motionX;
-    motionYprev = motionY;
-    motionZprev = motionZ;
+      motionXprev = motionX;
+      motionYprev = motionY;
+      motionZprev = motionZ;
+    }
+    event_counter += 1;
   }
   /**
   *
   * @param event
   */
   function deviceorientationHandler(event) {
-    //ジャイロセンサー情報取得
-    // X軸
-    beta = event.beta;
-    // Y軸
-    gamma = event.gamma;
-    // Z軸
-    alpha = event.alpha;
-    var html = "";
-    html += "X回転 : " + beta + "<br>";
-    html += "Y回転 : " + gamma + "<br>";
-    html += 'Z回転 : ' + alpha;
-    $("#debug").html(html);
+    if (event_counter%event_interval ==0){
+      //ジャイロセンサー情報取得
+      // X軸
+      beta = event.beta;
+      // Y軸
+      gamma = event.gamma;
+      // Z軸
+      alpha = event.alpha;
+      var html = "";
+      html += "X回転 : " + beta + "<br>";
+      html += "Y回転 : " + gamma + "<br>";
+      html += 'Z回転 : ' + alpha;
+      $("#debug").html(html);
 
-    socket.emit('sensor_update', ["orientation", beta, gamma, alpha]);
+      socket.emit('sensor_update', ["orientation", beta, gamma, alpha]);
+    }
+    event_counter += 1;
 
     $logo.css({
       "-webkit-transform": "rotateX(" + (135 + beta) + "deg) rotateY(" + (180 + gamma) + "deg) rotateZ(" + (180 + alpha) + "deg)",
