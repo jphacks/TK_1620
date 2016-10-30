ee588f7a1a855e12608858dda8d7acb6091b9140
Nymphaea808 <gingersyrup808@gmail.com>
2016-10-30 12:25:33 +0900

    mod p5main

--- LifeCanvas/public/js/p5main.js
+++ LifeCanvas/public/js/p5main.js
@@ -11,6 +11,7 @@ var _frameMaskBl;
 var _frameMaskWh;
 var _frameMaskCol; // フレーム更新時のマスク
 var size = 0;
+var _pointer;
 
 function setup(){
   createCanvas(_canvasSize,_canvasSize);
@@ -20,30 +21,66 @@ function setup(){
   _frameMaskBl=color('hsba(1,1%,0%,0.035)');
   _frameMaskWh=color('hsba(1,1%,100%,0.035)');
   _frameMaskCol=_frameMaskBl;
+  _pointer=new Pointer();
 
   // パーティクルのインスタンスを生成する
   for (var i=0; i<_particleNum; i++) {
     _particles.push(new Particle());
   }
 
-  mouseX=width/2
-  mouseY=height/2;
+  _pointer.pos.x=width/2;
+  _pointer.pos.y=height/2;
 
 
-  gbXox=XOX('x.....x.....x...');
-  gbPluck = Pluck();
-  gbDelay=Delay();
-  gbSeq=Seq({
-    time: Rndi( ms(2), ms(1000) ),
-    durations:1/32,
-    target: gbDelay
-  });
-  gbPluck.note.seq( [0, 2], [1/8,1/16].rnd(1/16,2) ).pan.seq(Rndf(-0.8,0.8)).damping(0.3).fx.add(Schizo());
-  gbPluck.blend.seq( Rndf(0.8,1) );
-  gbPluck.fx.add(gbDelay);
+  // gbXox=XOX('x.....x.....x...');
+  // gbPluck = Pluck();
+  // gbDelay=Delay();
+  // gbSeq=Seq({
+  //   time: Rndi( ms(2), ms(1000) ),
+  //   durations:1/32,
+  //   target: gbDelay
+  // });
+  // gbPluck.note.seq( [0, 2], [1/8,1/16].rnd(1/16,2) ).pan.seq(Rndf(-0.8,0.8)).damping(0.3).fx.add(Schizo());
+  // gbPluck.blend.seq( Rndf(0.8,1) );
+  // gbPluck.fx.add(gbDelay);
+  //
+  // gbBass = Mono( 'bass2' ,{waveform:'Square'}).amp(.1).note.seq( [0, 2], [1/8,1/16].rnd(1/16,2) );
+  // gbPad = Synth2( 'pad2', { amp:.85 } ).chord.seq( Rndi(0,2,9), 2 ).fx.add( Delay() );
 
-  gbBass = Mono( 'bass2' ,{waveform:'Square'}).amp(.1).note.seq( [0, 2], [1/8,1/16].rnd(1/16,2) );
-  gbPad = Synth2( 'pad2', { amp:.85 } ).chord.seq( Rndi(0,2,9), 2 ).fx.add( Delay() );
+  gbXox=XOX('x.*...x.**..x...');
+ gbXox.fx.add(
+   Crush({
+     bitDepth: 1+15*_pointer.pos.y/height,
+     sampleRate: _pointer.pos.x/width
+   })
+ );
+
+ xoxFollow=Follow(gbXox);
+
+ gbPluck = Pluck();
+ gbPluck.fx.add(
+   Delay({
+     time: 50*_pointer.pos.x/width,
+     feedback: 2*_pointer.pos.y/height,
+     durations:1/8
+   })
+ );
+
+ gbPluck.note.seq( [0, 2], [1/8,1/16].rnd(1/16,2) ).pan.seq(Rndf(-0.8,0.8)).damping(0.8).fx.add(Schizo());
+ gbPluck.blend.seq( Rndf(0.8,1) );
+
+ gbBass = Mono( 'bass2' ,{waveform:'Square'}).amp(.1).note.seq( [0, 2], [1/8,1/16].rnd(1/16,2) ).fx.add(
+   Flanger({
+     rate: 0.1+19.9*_pointer.pos.x/width,
+     amount: 200*_pointer.pos.y/height
+   })
+ );
+ gbPad = Synth2( 'pad2', { amp:.85 } ).chord.seq( Rndi(0,2,9), 2 ).fx.add(
+   Reverb({
+     roomSize: .99,
+     damping: 0.9
+   })
+ );
 
 }
 
@@ -60,11 +97,12 @@ function draw(){
 
   socket.on('sensor_data', function(data){
     if (data[0] == "acceleration"){
-      size = data[1]*100;
+      //size = data[1]*100;
+      _pointer.update(data[1],data[2]);
     }
-    console.log(data);
+    //console.log(data);
   });
-  ellipse(width/2, height/2, size, size);
+  //ellipse(width/2, height/2, size, size);
 
 }
 
@@ -80,6 +118,26 @@ function keyTyped() {
   } else if(key=='s'){
     for(var i=0;i<_particles.length;i++){
       _particles[i].mirror();
+    }
+  } else if(key=='1'){
+    _weather="SUNNY";
+    for(var i=0;i<_particles.length;i++){
+      _particles[i].colChange();
+    }
+  }else if(key=='2'){
+    _weather="RAINY";
+    for(var i=0;i<_particles.length;i++){
+      _particles[i].colChange();
+    }
+  }else if(key=='3'){
+    _weather="CLOUDY";
+    for(var i=0;i<_particles.length;i++){
+      _particles[i].colChange();
+    }
+  }else if(key=='4'){
+    _weather="SNOWY";
+    for(var i=0;i<_particles.length;i++){
+      _particles[i].colChange();
     }
   }
   else {
@@ -134,7 +192,7 @@ Particle.prototype={
       this.strokeW=this.initStrokeW;
     }
 
-    attraction=createVector(mouseX,mouseY);
+    attraction=createVector(_pointer.pos.x,_pointer.pos.y);
     attraction.sub(this.pos);
     this.ac.set(attraction);
     this.ac.normalize();
@@ -163,7 +221,7 @@ Particle.prototype={
       this.ac.y*=-1;
     }
 
-    //attraction=createVector(mouseX,mouseY);
+    //attraction=createVector(_pointer.pos.x,_pointer.pos.y);
     //attraction.sub(this.pos);
     //this.ac.set(attraction);
     this.ac.normalize();
@@ -180,6 +238,25 @@ Particle.prototype={
   mirror : function(){
     this.pos.set(width-this.pos.x,height-this.pos.y);
     // this.pos.y=height-this.pos.y;
+  },
+  colChange: function(){
+    switch(_weather){
+      case "SUNNY":
+      this.col=color(random(320,420)%360,100,100);
+      break;
+      case "RAINY":
+      this.col=color(random(170,250)%360,100,100);
+      break;
+      case "CLOUDY":
+      this.col=color(0,0,round(random(30,100)));
+      break;
+      case "SNOWY":
+      this.col=color(208,round(random(0,100)),100);
+      break;
+      default:
+      this.col=color(random(360),90,100);
+      break;
+    }
   }
 };
 
@@ -190,6 +267,36 @@ function hamon(xPos, yPos, size, num, interval, col) {
     ellipse(xPos, yPos, size + interval * i);
   }
 }
+
+var Pointer=function(){
+  this.pos=createVector(0,0);
+
+  this.ac=createVector(0,0);
+};
+
+Pointer.prototype={
+  update : function(x,y){
+    //this.pre=createVector(this.pos.x,this.pos.y);
+    if(this.pos.x>width || this.pos.x<0){
+      this.ac.x*=-1.0;
+    }
+    if(this.pos.y<0||this.pos.y>height){
+      this.sp.y*=-1.0;
+    }
+    this.pos.ac.x=x;
+    this.pos.ac.y=y;
+    // attraction=createVector(_pointer.pos.x,_pointer.pos.y);
+    // attraction.sub(this.pos);
+    // this.ac.set(attraction);
+    // this.ac.normalize();
+    // this.ac.div(this.tune);
+    // this.sp.add(this.ac);
+    // this.sp.limit(_sLimit*(1.0/60.0));
+    // this.pos.add(this.sp);
+    this.pos.x+=this.ac.x;
+    this.pos.y+=this.ac.y;
+  }
+};
 
 
 // var Particle=(function(){
@@ -225,8 +332,8 @@ function hamon(xPos, yPos, size, num, interval, col) {
 //       if(this.pos.y<0||pos.y>height){
 //         this.sp*=-0.9;
 //       }
-//       this.mouse.x=mouseX;
-//       this.mouse.y=mouseY;
+//       this.mouse.x=_pointer.pos.x;
+//       this.mouse.y=_pointer.pos.y;
 //       var attraction=createVector(this.mouse.x,this.mouse.y);
 //       attraction.sub(this.pos);
 //       this.ac.copy(attraction);
